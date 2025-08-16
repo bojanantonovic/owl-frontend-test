99dc679e03b3967cbdfbd48f978a5c1030f2866a
Bojan Antonovic <bojan@antonovic.ch>
2025-08-16 10:45:05 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms'; // 👈 here
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app',
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('owl-frontend-test');
+  // login properties
+  isLoggedIn = signal(false);
+  credentialsInvalid = signal(false);
+  //name = model('');
+  //password = model('');
+  name = '';
+  password = '';
+  validCredentials: string[][] = [['Donald', 'Duck'], ['Mickey', 'Mouse']];
+
+  userName = signal('');
+
+  onSubmit() {
+    //this.isLoggedIn.set(true);
+    //this.userName.set(this.name());
+    //   if (this.validCredentials.includes([this.name, this.password])) {
+    if (this.validCredentials.some(
+      credentials => credentials[0] === this.name && credentials[1] === this.password)) {
+      this.isLoggedIn.set(true);
+      this.userName.set(this.name);
+      this.credentialsInvalid.set(false);
+    } else {
+      this.credentialsInvalid.set(true);
+    }
+  }
 }
